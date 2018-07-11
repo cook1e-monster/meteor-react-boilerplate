c2a799796dd30d832be15232af7ffc361a12cfb6
Carlos silva garcia <carlossil01@gmail.com>
2018-07-11 12:10:11 +0700

    update register api

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -1,6 +1,19 @@
 // Register your apis here
+
+// --- configurations ---------------------------
+import './jobs.js'
 import '/imports/api/logger/server/logger.js'
 import './config.js'
 
 //publications
 import '/imports/api/accounts/server/publications.js'
+import '/imports/api/jobs/server/publications.js'
+
+//graph
+// import '/imports/api/Posts/server/graph.js'
+
+//jobs
+import '/imports/api/jobs/server/jobs.js'
+
+//methods
+import '/imports/api/jobs/server/methods.js'
